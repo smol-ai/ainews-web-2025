4589a50b072b626454cda0ba08cbe07a660f9a5f
swyxio <shawnthe1@gmail.com>
2025-04-23 16:17:02 -0700

    fix pagefind tagcloud " "

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,13 +3,13 @@ import { SITE } from "@consts";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
+  const issues = (await getCollection("issues")).filter((post) => !post.data.draft);
 
   const projects = (await getCollection("projects")).filter(
     (project) => !project.data.draft,
   );
 
-  const items = [...blog, ...projects].sort(
+  const items = [...issues, ...projects].sort(
     (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
   );
 
